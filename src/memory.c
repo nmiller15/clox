321f634005173ac2c80488734bbc8f839b1840ef
Nolan Miller <nolan.miller77@gmail.com>
2025-11-24 05:59:50 -0500

    add functions and function calls

--- src/memory.c
+++ src/memory.c
@@ -17,6 +17,16 @@ void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
 
 static void freeObject(Obj* object) {
     switch(object->type) {
+        case OBJ_FUNCTION: {
+            ObjFunction* function = (ObjFunction*) object;
+            freeChunk(&function->chunk);
+            FREE(ObjFunction, object);
+            break;
+        }
+        case OBJ_NATIVE: {
+            FREE(ObjNative, object);
+            break;
+        }
         case OBJ_STRING: {
             ObjString* string = (ObjString*) object;
             FREE_ARRAY(char, string->chars, string->length + 1);
@@ -28,7 +38,7 @@ static void freeObject(Obj* object) {
 
 void freeObjects() {
     Obj* object = vm.objects;
-    while (vm.objects != NULL) {
+    while (object != NULL) {
         Obj* next = object->next;
         freeObject(object);
         object = next;

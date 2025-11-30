be4df6d426249f3005c933cbd4192d6b73d72f86
Nolan Miller <nolan.miller77@gmail.com>
2025-11-30 12:16:48 -0500

    wip add garbage collector

--- src/table.c
+++ src/table.c
@@ -128,3 +128,11 @@ ObjString* tableFindString(Table* table, const char* chars,
     }
 }
 
+void markTable(Table* table) {
+    for (int i = 0; i < table->capacity; i++) {
+        Entry* entry = &table->entries[i];
+        markObject((Obj*)entry->key);
+        markValue(entry->value);
+    }
+}
+
